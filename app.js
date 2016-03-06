367616b30154e5697f0d83f0be01829cf4c35bda
Ankit Balyan <ankit.kr.balyan@gmail.com>
2016-03-06 21:19:27 +0530

    added nedb and queries to db

--- app.js
+++ app.js
@@ -15,19 +15,19 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 app.get('/',function(req, res) {
 	// res.send("Helo World!");
-	res.render('index', {
-		mappings: mappings 
-	})
+	mappings.list(function(err, mappingDocuments) {
+		res.render('index', {
+			mappings: mappingDocuments
+		})
+	});
 });
 
 app.get('/:alias',function (req, res) {
 	mappings.get(req.params.alias,function(err, mapping) {
 		if(err){
-			res.sendStatus(404);
-			return res.end();
+			return res.sendStatus(404);
 		}
-		res.redirect(mapping);
-		res.end();
+		return res.redirect(mapping);
 	});
 });
 
--- mappings.js
+++ mappings.js
@@ -1,16 +1,31 @@
 'user strict';
+var path = require('path'),
+	
+	Database = require('nedb');
 
-var data = {
-	g: 'http://google.com'
-};
+// var data = {
+// 	g: 'http://google.com'
+// };
+
+var db = {
+	mappings: new Database({filename: path.join(__dirname,'mappings.db'),autoload: true})
+}
 
 var mappings = {
 	get: function (alias,callback) {
-		// var alias = url.substring(1);
-		if(!data[alias]){
-			return callback(new Error("Url not found"));
-		}
-		return callback(null,data[alias]);
+		db.mappings.findOne({alias: alias},function(err, mapping) {
+			// var alias = url.substring(1);
+			if(err || !mapping){
+				return callback(new Error("alias not found"));
+			}
+			return callback(null,mapping.url);
+		})
+	},
+	create: function(alias, url, callback) {
+		db.mappings.insert({'alias': alias, url: url},callback);
+	},
+	list:function(callback) {
+		db.mappings.find({}).sort({alias:1}).exec(callback);
 	}
 }
 module.exports = mappings;
